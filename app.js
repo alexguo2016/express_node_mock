37b0a5fd1e9dbb7da5e5cfd793352cfcb9647a67
alexguo2016 <240791268@qq.com>
2019-09-18 10:11:17 +0800

    跨域设置

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //设置跨域访问
-app.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+app.use((req, res, next) => {
+  // 设置是否运行客户端设置 withCredentials
+  // 即在不同域名下发出的请求也可以携带 cookie
+  res.header("Access-Control-Allow-Credentials",true)
+  // 第二个参数表示允许跨域的域名，* 代表所有域名  
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
+  // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+  if (req.method == 'OPTIONS') {
+      res.sendStatus(200)
+  } else {
+      next()
+  }
+})
 
 // 读取文件的工具函数
 var myTools = require('./util.js')
